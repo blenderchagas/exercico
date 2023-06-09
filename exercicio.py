bfeef9132a467b7bd3c47e6460a0fe6e773c38bf
Blênder Chagas <blenderchagas@hotmail.com.br>
2023-06-09 11:39:35 -0300

    completo

--- exercicio.py
+++ exercicio.py
@@ -1,14 +1,25 @@
-M: int
-N: int
-M = int(input("Quantas linhas vai ter a matriz? "))
-N = int(input("Quantas colunas vai ter a matriz? "))
-mat: [[int]] = [[0 for x in range(N)] for x in range(M)]
-for i in range(0, M):
- for j in range(0, N):
-  mat[i][j] = int(input(f"Elemento [{i},{j}]: "))
-print()
-print("MATRIZ DIGITADA:")
-for i in range(0, M):
- for j in range(0, N):
-  print(f"{mat[i][j]} ", end="")
-print()
+m: int; n: int
+somalinha: float
+
+m = int(input("Qual a quantidade de linhas da matriz? "))
+n = int(input("Qual a quantidade de colunas da matriz? "))
+
+matriz: [[float]] = [[0 for x in range(n)] for x in range(m)]
+vetor: [float] = [0 for x in range(m)]
+
+for i in range(m):
+	print(f"Digite os elementos da {i + 1} a. linha")
+	for j in range(n):
+		matriz[i][j] = float(input())
+
+for i in range(m):
+	somalinha = 0
+
+	for j in range(n):
+		somalinha = somalinha + matriz[i][j]
+	vetor[i] = somalinha
+
+print("VETOR GERADO:")
+
+for i in range(m):
+	print(f"{vetor[i]:.1f}")
